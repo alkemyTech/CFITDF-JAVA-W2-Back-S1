b478e3bfa6c3092987e4f0e1802b311de7f2f726
Jonathan Vera <jonii10lea@gmail.com>
2025-05-15 20:01:24 -0300

    Agregando DTO y Repository

---
+++ src/main/java/com/alkemy/wallet/alkywallet/dto/UsuarioDTO.java
@@ -0,0 +1,41 @@
+package com.alkemy.wallet.alkywallet.dto;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class UsuarioDTO {
+
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    private boolean accountNonLocked;
+    private boolean accountNonExpired;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+    private List<Cuenta> cuentas = new ArrayList<>();
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(Long id, String nombre, String apellido, String email, String password, boolean accountNonLocked, boolean accountNonExpired, boolean credentialsNonExpired, boolean enabled, List<Cuenta> cuentas) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.password = password;
+        this.accountNonLocked = accountNonLocked;
+        this.accountNonExpired = accountNonExpired;
+        this.credentialsNonExpired = credentialsNonExpired;
+        this.enabled = enabled;
+        this.cuentas = cuentas;
+    }
+}
---
+++ src/main/java/com/alkemy/wallet/alkywallet/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.alkemy.wallet.alkywallet.repository;
+
+import com.alkemy.wallet.alkywallet.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
