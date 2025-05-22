ef6bd8f8274608c47e8449e485be121415586482
Matias Perez <149439383+matiasperez01@users.noreply.github.com>
2025-05-21 23:03:01 -0300

    Revert "Tarjeta"

--- src/main/java/com/alkemy/wallet/alkywallet/model/TipoTransaccion.java
+++
@@ -1,8 +0,0 @@
-package com.alkemy.wallet.alkywallet.model;
-
-public enum TipoTransaccion {
-    DEPOSITO,
-    EXTRACCION,
-    TRANSFERENCIA,
-    PAGO
-}
--- src/main/java/com/alkemy/wallet/alkywallet/repository/TransaccionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alkemy.wallet.alkywallet.repository;
-
-import com.alkemy.wallet.alkywallet.model.Transaccion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransaccionRepository extends JpaRepository <Transaccion, Long>{
-}
