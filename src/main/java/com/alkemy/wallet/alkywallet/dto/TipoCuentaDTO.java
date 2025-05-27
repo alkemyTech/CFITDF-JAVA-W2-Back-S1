97d59e96c965f382f39698832b48be84d4aeafac
Hectorlag <hectorvlaguna@gmail.com>
2025-05-27 19:02:45 -0300

    feat: agregar endpoint de tipos de cuenta

---
+++ src/main/java/com/alkemy/wallet/alkywallet/dto/TipoCuentaDTO.java
@@ -0,0 +1,16 @@
+package com.alkemy.wallet.alkywallet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TipoCuentaDTO {
+
+    private String nombre;
+    private String descripcion;
+}
