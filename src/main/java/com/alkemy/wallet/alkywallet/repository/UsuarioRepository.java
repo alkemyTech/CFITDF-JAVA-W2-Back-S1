1c151c2d45e3f5ba9074dec0577fa6c9ebf8e146
MelinaFigueroa <138631344+MelinaFigueroa@users.noreply.github.com>
2025-05-29 12:04:38 -0300

    Update UsuarioRepository.java

--- src/main/java/com/alkemy/wallet/alkywallet/repository/UsuarioRepository.java
+++ src/main/java/com/alkemy/wallet/alkywallet/repository/UsuarioRepository.java
@@ -2,25 +2,16 @@ package com.alkemy.wallet.alkywallet.repository;
 
 import com.alkemy.wallet.alkywallet.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-import org.springframework.stereotype.Repository;
-
-=======
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
->>>>>>> dev
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-<<<<<<< HEAD
-    Optional<Usuario> findByEmail(String email);
-=======
     Optional<Usuario> findByIdAndBorradoFalse(Long id);
     Optional<Usuario> findByEmailAndBorradoFalse(String email);
     List<Usuario> findByBorradoFalse();
->>>>>>> dev
 }
