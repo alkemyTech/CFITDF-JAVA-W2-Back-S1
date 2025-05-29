697310196c1c1b1da603f0d8db69cf610484e805
MelinaFigueroa <138631344+MelinaFigueroa@users.noreply.github.com>
2025-05-29 12:02:42 -0300

    Update TarjetaRepository.java

--- src/main/java/com/alkemy/wallet/alkywallet/repository/TarjetaRepository.java
+++ src/main/java/com/alkemy/wallet/alkywallet/repository/TarjetaRepository.java
@@ -1,14 +1,5 @@
 package com.alkemy.wallet.alkywallet.repository;
 
-<<<<<<< HEAD
-import com.alkemy.wallet.alkywallet.model.Tarjeta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TarjetaRepository extends JpaRepository<Tarjeta, Long> {
-
-=======
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +15,4 @@ public interface TarjetaRepository extends JpaRepository<Tarjeta, Long> {
 	public List<Tarjeta> findByDeletedFalse();
 
 	public List<Tarjeta> findByUsuarioIdAndDeletedFalse(Long id);
->>>>>>> dev
 }
