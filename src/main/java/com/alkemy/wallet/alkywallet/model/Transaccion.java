739dc9f14f65742c3558b41fc34a26cc5b18258e
MelinaFigueroa <138631344+MelinaFigueroa@users.noreply.github.com>
2025-05-29 11:57:32 -0300

    Update Transaccion.java

--- src/main/java/com/alkemy/wallet/alkywallet/model/Transaccion.java
+++ src/main/java/com/alkemy/wallet/alkywallet/model/Transaccion.java
@@ -1,19 +1,11 @@
 package com.alkemy.wallet.alkywallet.model;
 
 import jakarta.persistence.*;
-<<<<<<< HEAD
-import jakarta.validation.constraints.*;
-=======
 import jakarta.validation.constraints.NotNull;
->>>>>>> dev
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-<<<<<<< HEAD
-
-=======
->>>>>>> dev
 import java.time.LocalDate;
 
 @Entity
@@ -28,21 +20,10 @@ public class Transaccion {
     private long id;
 
     @Temporal(TemporalType.DATE)
-<<<<<<< HEAD
-    @NotNull(message = "La fecha es obligatoria")
-    private LocalDate fecha;
-
-    @NotNull(message = "El monto es obligatorio")
-    @Positive(message = "El monto debe ser un valor positivo")
-    private Double monto;
-
-    @NotBlank(message = "La descripción es obligatoria")
-=======
     private LocalDate fecha;
 
     private double monto;
 
->>>>>>> dev
     private String descripcion;
 
     @Enumerated(EnumType.STRING)
@@ -51,11 +32,5 @@ public class Transaccion {
 
     @ManyToOne
     @JoinColumn(name = "tarjeta_id")
-<<<<<<< HEAD
-    @NotNull(message = "La tarjeta es obligatoria")
     private Tarjeta tarjeta;
 }
-=======
-    private Tarjeta tarjeta;
-}
->>>>>>> dev
