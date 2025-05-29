92b9a545794df318943e445f1b74d0d6354b877c
MelinaFigueroa <138631344+MelinaFigueroa@users.noreply.github.com>
2025-05-29 12:04:12 -0300

    Update TransaccionRepository.java

--- src/main/java/com/alkemy/wallet/alkywallet/repository/TransaccionRepository.java
+++ src/main/java/com/alkemy/wallet/alkywallet/repository/TransaccionRepository.java
@@ -1,6 +1,5 @@
 package com.alkemy.wallet.alkywallet.repository;
 
-<<<<<<< HEAD
 import com.alkemy.wallet.alkywallet.model.TipoTransaccion;
 import com.alkemy.wallet.alkywallet.model.Transaccion;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,10 +17,10 @@ public interface TransaccionRepository extends JpaRepository<Transaccion, Long> 
 
     // Listar transacciones por tarjeta
     List<Transaccion> findByTarjetaId(Long tarjetaId);
-=======
+
 import com.alkemy.wallet.alkywallet.model.Transaccion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransaccionRepository extends JpaRepository <Transaccion, Long>{
->>>>>>> dev
+
 }
