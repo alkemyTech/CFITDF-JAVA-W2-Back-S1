4acbbafe8c735ee6c32f7e4c99616c2c3dd59847
MelinaFigueroa <138631344+MelinaFigueroa@users.noreply.github.com>
2025-05-29 11:59:10 -0300

    Update Cuenta.java

--- src/main/java/com/alkemy/wallet/alkywallet/model/Cuenta.java
+++ src/main/java/com/alkemy/wallet/alkywallet/model/Cuenta.java
@@ -1,9 +1,5 @@
 package com.alkemy.wallet.alkywallet.model;
 
-<<<<<<< HEAD
-
-import jakarta.persistence.*;
-=======
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -20,7 +16,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
->>>>>>> dev
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
@@ -28,11 +23,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-<<<<<<< HEAD
-import java.util.List;
-
-=======
->>>>>>> dev
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -41,42 +31,6 @@ import java.util.List;
 @Table(name = "cuentas")
 public class Cuenta {
 
-<<<<<<< HEAD
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-
-        @PositiveOrZero(message = "El saldo no puede ser negativo")
-        private Double saldo;
-
-        @NotNull(message = "El tipo de cuenta es obligatorio")
-        @Enumerated(EnumType.STRING)
-        private TipoCuenta tipo;
-
-        // Unidireccional hacia Usuario
-        @NotNull(message = "La cuenta debe estar asociada a un usuario")
-        @ManyToOne
-        @JoinColumn(name = "usuario_id", nullable = false)
-        private Usuario usuario;
-
-        // Unidireccional hacia Transaccion
-        @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-        @JoinColumn(name = "cuenta_id")
-        private List<Transaccion> transacciones;
-
-        // Unidireccional hacia Pago
-        @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-        @JoinColumn(name = "cuenta_id")
-        private List<Pago> pagos;
-
-        // Unidireccional hacia Tarjeta
-        @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-        @JoinColumn(name = "cuenta_id")
-        private List<Tarjeta> tarjetas;
-
-        @Column(nullable = false)
-        private boolean deleted = false;
-=======
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -113,5 +67,4 @@ public class Cuenta {
 	@Column(nullable = false)
 	private boolean deleted = false;
 
->>>>>>> dev
 }
