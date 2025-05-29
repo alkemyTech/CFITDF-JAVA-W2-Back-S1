6dc397013f351bd2686a2ab4ba4d485d5fb44ddf
MelinaFigueroa <138631344+MelinaFigueroa@users.noreply.github.com>
2025-05-29 11:57:55 -0300

    Update TipoTransaccion.java

--- src/main/java/com/alkemy/wallet/alkywallet/model/TipoTransaccion.java
+++ src/main/java/com/alkemy/wallet/alkywallet/model/TipoTransaccion.java
@@ -1,26 +1,8 @@
 package com.alkemy.wallet.alkywallet.model;
 
 public enum TipoTransaccion {
-<<<<<<< HEAD
-    DEPOSITO("Depósito"),
-    EXTRACCION("Extracción"),
-    TRANSFERENCIA("Transferencia"),
-    PAGO("Pago");
-
-    private final String descripcion;
-
-    private TipoTransaccion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getTipo() {
-        return descripcion;
-    }
-}
-=======
     DEPOSITO,
     EXTRACCION,
     TRANSFERENCIA,
     PAGO
 }
->>>>>>> dev
