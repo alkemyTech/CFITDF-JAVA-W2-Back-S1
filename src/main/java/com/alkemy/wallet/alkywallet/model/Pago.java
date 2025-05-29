4d087c9a908530b5ecd2bc7f2697aae1dbdb4c68
MelinaFigueroa <138631344+MelinaFigueroa@users.noreply.github.com>
2025-05-29 11:58:50 -0300

    Update Pago.java

--- src/main/java/com/alkemy/wallet/alkywallet/model/Pago.java
+++ src/main/java/com/alkemy/wallet/alkywallet/model/Pago.java
@@ -1,9 +1,6 @@
 package com.alkemy.wallet.alkywallet.model;
 
-<<<<<<< HEAD
-=======
 import com.fasterxml.jackson.annotation.JsonIgnore;
->>>>>>> dev
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,11 +9,8 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-<<<<<<< HEAD
-=======
 import java.util.Date;
 
->>>>>>> dev
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -34,20 +28,12 @@ public class Pago {
 
     private String comercio;
 
-<<<<<<< HEAD
-    @Temporal(TemporalType.DATE)
-    private LocalDate fecha;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cuenta_id", nullable = false)
-=======
     @Temporal(TemporalType.TIMESTAMP)
     private Date fecha;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cuenta_id", nullable = false)
     @JsonIgnore
->>>>>>> dev
     private Cuenta cuenta;
 
     @Override
@@ -57,10 +43,6 @@ public class Pago {
                 ", monto=" + monto +
                 ", comercio='" + comercio + '\'' +
                 ", fecha=" + fecha +
-<<<<<<< HEAD
-                ", cuentaId=" + (cuenta != null ? cuenta.getId() : null) +
-=======
->>>>>>> dev
                 '}';
     }
 }
